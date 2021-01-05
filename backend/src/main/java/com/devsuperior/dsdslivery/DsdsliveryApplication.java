eb35fd66705659aa095c04397950827ab522547e
Gabriel G81 <gabitwo81@gmail.com>
2021-01-05 10:02:25 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devsuperior/dsdslivery/DsdsliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsdslivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsdsliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsdsliveryApplication.class, args);
+	}
+
+}
