9b38d9bd14218679904e0835bb26ca37498702e2
Gabriel G81 <gabitwo81@gmail.com>
2021-01-08 18:13:08 -0300

    GET/orders

---
+++ backend/src/main/java/com/devsuperior/dsdslivery/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsdslivery.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsdslivery.dto.OrderDTO;
+import com.devsuperior.dsdslivery.entities.Order;
+import com.devsuperior.dsdslivery.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<OrderDTO> findAll() {
+		List<Order> list = repository.findOrdersWithProducts();
+		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
+	}
+
+}
